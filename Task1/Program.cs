72f0e3116568b2e9c030bbbccd8b680373351759
Yana <nikandrova0915@gmail.com>
2022-12-10 22:35:00 +0300

    Задача1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+void FindSquare(int number1, int number2)
+{
+    double result = Math.Pow(number1,number2); 
+    Console.Write($"Число {number1} в степени {number2} равно {result}");
+}
+
+
+
+int EnterData(string text)
+{
+   Console.Write(text);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+
+
+int number1 = EnterData("Введите первое число: ");
+int number2 = EnterData("Введите второе число: ");
+FindSquare(number1,number2);
