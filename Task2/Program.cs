e6f1b5b4f73d1a4e99fb6f3c202277d44daf4218
Yana <nikandrova0915@gmail.com>
2022-12-15 08:44:17 +0300

    Задача2

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход 
+// число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+void SumInNumb(int numb)
+{
+    string remake = numb.ToString();
+    int legthNumb = remake.Length;
+    int [] var = new int [legthNumb];
+    int result = 0;
+    for(int i = 0; i < legthNumb; i++)
+    {
+        var[i] = int.Parse(remake[i].ToString());
+        result = result + var[i];
+    }
+    Console.WriteLine($"Сумма цифр числа {numb} равна {result}");
+}
+int EnterNumb(string text)
+{
+    Console.Write(text);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+int numb = EnterNumb("Введите число: ");
+SumInNumb(numb);
